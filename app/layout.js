3524d4421ef888d25261ed39d32c8105769e1aca
Remon Tripura <121652185+remontripura@users.noreply.github.com>
2025-09-20 17:45:25 +0600

    udpate

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
-import { Tiro_Bangla } from 'next/font/google'
-import './globals.css'
+import { Tiro_Bangla } from "next/font/google";
+import "./globals.css";
+import { Provider } from "./_components/_provider/Provider";
 
-const tiro = Tiro_Bangla({ subsets: ['latin'], weight: "400" })
+const tiro = Tiro_Bangla({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
-  title: 'Good Morning Aid',
-  description: 'Good Morning Aid a Trusted Medical Shop',
-}
+  title: "Good Morning Aid",
+  description: "Good Morning Aid a Trusted Medical Shop",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>   <link rel="icon" href="https://i.postimg.cc/PJ8b3Msv/gmfav.png" /></head>
-      <body className={tiro.className}>{children}</body>
+      <head>
+        {" "}
+        <link rel="icon" href="https://i.postimg.cc/PJ8b3Msv/gmfav.png" />
+      </head>
+      <body className={tiro.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
-  )
+  );
 }
